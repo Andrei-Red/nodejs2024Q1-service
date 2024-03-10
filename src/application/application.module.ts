1a282b1b3b8a023196ef934acb6b40c91438065b
Andrei_Dzenicenko <denisenko.andrei.da@gmail.com>
2024-03-10 21:05:59 +0300

    impl: add others service

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ArtistService } from './artist/artist.service';
+import { AlbumsService } from './albums/albums.service';
+import { TracksService } from './tracks/tracks.service';
+import { FavoritesService } from './favorites/favorites.service';
 
 @Module({
   imports: [UserModule],
+  providers: [ArtistService, AlbumsService, TracksService, FavoritesService],
 })
 export class ApplicationModule {}
