8495b252ee445cb5cb8e7fc75d5edbc9a379510e
Andrei_Dzenicenko <denisenko.andrei.da@gmail.com>
2024-03-10 21:07:31 +0300

    impl: add others controllers

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -4,9 +4,14 @@ import { ArtistService } from './artist/artist.service';
 import { AlbumsService } from './albums/albums.service';
 import { TracksService } from './tracks/tracks.service';
 import { FavoritesService } from './favorites/favorites.service';
+import { FavoritesController } from './favorites/favorites.controller';
+import { TracksController } from './tracks/tracks.controller';
+import { AlbumsController } from './albums/albums.controller';
+import { ArtistsController } from './artists/artists.controller';
 
 @Module({
   imports: [UserModule],
   providers: [ArtistService, AlbumsService, TracksService, FavoritesService],
+  controllers: [FavoritesController, TracksController, AlbumsController, ArtistsController],
 })
 export class ApplicationModule {}
